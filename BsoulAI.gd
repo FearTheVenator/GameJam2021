b26ee8503ef890b469e14a6e3e20af66efba445e
AllanNixon <60796736+AllanNixon@users.noreply.github.com>
2021-01-30 00:17:13 -0600

    Add files via upload

--- BsoulAI.gd
+++ BsoulAI.gd
@@ -1,16 +1,26 @@
 extends KinematicBody
 
+var path = []
+var path_node = 0
+var speed = 10
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var nav = get_parent()
+onready var player = $"../../GoodGhost"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
+func _physics_process(delta):
+	if path_node < path.size():
+		var direction = (path[path_node] - global_transform.origin)
+		if direction.length() < 1:
+			path_node += 1
+		else:
+			move_and_slide(direction.normalized() * speed, Vector3.UP)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func move_to(target_pos):
+	path = nav.get_simple_path(global_transform.orgin, target_pos)
+	path_node = 0
+
+func _on_Timer_timeout():
+	move_to(player.global_transform.origin)
