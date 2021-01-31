f4e7a4964c13270f25fd67260486b90c8ab0b8be
Baran <erfani.baran@outlook.com>
2021-01-31 14:14:29 -0600

    Revert "Merge branch 'main' of https://github.com/FearTheVenator/GameJam2021 into main"
    This reverts commit 639ae36bb49fdb50466ea02f3b048437b6f73b9f, reversing
    changes made to 0bd9194d79155257aa2b52fa5c1b587b36edef2d.

--- Scripts/SoulPathing.gd
+++ Scripts/SoulPathing.gd
@@ -3,21 +3,16 @@ extends KinematicBody
 var path =[]
 var path_node = 0
 var speed = 5
-
 #Gets the parent node: the kinematic soul
 onready var nav = get_parent()
 #This is the node that we are tracking it can be a player a cube a 3d position and so
 #Ideally we will create an array of positions for the Ghost to path to
 onready var goalPosition = get_node("/root/Maze/Goal")
-
-
-
 func _ready():
 	pass
 func _physics_process(delta):
 #This checks how many godot units away from its objective is
 #The path actually places multiple node waypoints to figure out where its going
-	
 	if path_node < path.size():
 		var direction = (path[path_node] - global_transform.origin)
 		if direction.length() < 1:
